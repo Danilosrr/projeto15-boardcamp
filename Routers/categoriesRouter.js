0644c43bc3f7b733a3f93fa9e9a378684851dbe0
Danilo <danilo_srr@hotmail.com>
2022-05-29 01:15:26 -0300

    categories controllers added

--- Routers/categoriesRouter.js
+++ Routers/categoriesRouter.js
@@ -1,8 +1,9 @@
 import { Router } from 'express';
+import { addCategories, listCategories } from '../Controllers/categoriesController.js';
 
 const categoriesRouter = Router();
 
-categoriesRouter.get('/categories', );
-categoriesRouter.post('/categories', );
+categoriesRouter.get('/categories', listCategories);
+categoriesRouter.post('/categories', addCategories);
 
 export default categoriesRouter;
