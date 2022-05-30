edcab280752020eed62dbad83dfd8d757351e6ec
Danilo <danilo_srr@hotmail.com>
2022-05-30 00:11:18 -0300

    rentals post implemented, Re-do

--- Routers/rentalsRouter.js
+++ Routers/rentalsRouter.js
@@ -1,9 +1,10 @@
 import { Router } from 'express';
+import { addRental } from '../Controllers/rentalsController.js';
 
 const rentalsRouter = Router();
 
 rentalsRouter.get('/rentals', );
-rentalsRouter.post('/rentals',);
+rentalsRouter.post('/rentals',addRental);
 rentalsRouter.post('/rentals/:id/return',);
 rentalsRouter.delete('/rentals/:id',);
 
